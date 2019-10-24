9fc87198ddac9861d5a46e465db7bc660867d1f0
Lalith Sundar <51509309+lalith47@users.noreply.github.com>
2019-10-24 11:18:13 +0530

    Update admit.py
    added the check admin

--- mysite/Address/admin.py
+++ mysite/Address/admin.py
@@ -6,5 +6,9 @@ from .models import Address
 class AddressAdmin(admin.ModelAdmin):
     list_display=('street_address','city','state','pincode','country')
     list_filter=('city',)
-
+    
+    def CheckInAddress(name):
+        if name in list_display:
+            return "valid"
+        
 admin.site.register(Address,AddressAdmin)
